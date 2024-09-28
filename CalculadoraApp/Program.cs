b9fac3fd4f0cd2f2890a1e6a0b24b6d8e9603340
Lucas-RM <lucas.marcondes36@gmail.com>
2024-09-28 13:59:45 -0300

    Implementado a classe CalculadoraService

--- CalculadoraApp/Program.cs
+++ CalculadoraApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculadoraApp.Services;
+
+CalculadoraService calculadora = new CalculadoraService();
+
+int num1 = 5;
+int num2 = 10;
+
+Console.WriteLine($"{num1} + {num2} = {calculadora.Somar(num1, num2)}");
---
+++ CalculadoraApp/Services/CalculadoraService.cs
@@ -0,0 +1,10 @@
+namespace CalculadoraApp.Services
+{
+    public class CalculadoraService
+    {
+        public int Somar(int num1, int num2)        
+        {
+            return num1 + num2;
+        }
+    }
+}
