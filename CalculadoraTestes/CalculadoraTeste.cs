12177e2b5ebf59dc1bc3e8cb7d520e2512873088
Lucas-RM <lucas.marcondes36@gmail.com>
2024-09-28 14:55:43 -0300

    Implementando o teste unitário

--- CalculadoraTestes/CalculadoraTeste.cs
+++ CalculadoraTestes/CalculadoraTeste.cs
@@ -15,8 +15,14 @@ namespace CalculadoraTestes
         public void DeveSomar5Com10ERetornar15()
         {
             //Arrange
+            int num1 = 5;
+            int num2 = 10;
+
             //Act
+            int resultado = _calculadora.Somar(num1, num2);
+
             //Assert
+            Assert.Equal(15, resultado);
         }
     }
 }
